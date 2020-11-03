b98d6a80c542b03d0bd6a38c77b29bd054d4cad9
YashKandalkar <yashsk.yk@gmail.com>
2020-11-03 23:44:51 +0530

    Initial

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import { registerRootComponent } from 'expo';
+import { Provider as PaperProvider } from 'react-native-paper';
+
 
 import App from './App';
+
+const Main = () => {
+    return (
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    );
+  }
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Main);
